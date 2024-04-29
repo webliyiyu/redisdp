2363c451d96f7c2cd465d615b13dfb7d9da079d7
忆鱼 <3055615453@qq.com>
2024-04-29 17:36:14 +0800

    feat(RedisIdWorker): 完成了Redis实现全局唯一id
    Redis实现全局唯一id
    时间戳+序列号
    唯一性、高可用、高性能、递增性、安全性

--- hm-dianping/src/main/java/com/hmdp/utils/RedisIdWorker.java
+++ hm-dianping/src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -1,5 +1,6 @@
 package com.hmdp.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ import java.time.format.DateTimeFormatter;
 /**
  * ClassName: RedisIdWorker
  * Description:
+ *
  * Date: 2023/04/14 PM 12:58:12
  * <p>
  * project: hm-dianping
@@ -20,6 +22,7 @@ import java.time.format.DateTimeFormatter;
  * @author WangGuojian
  */
 @Component
+@Slf4j
 public class RedisIdWorker {
     /**
      * 开始时间戳
@@ -36,19 +39,28 @@ public class RedisIdWorker {
         this.stringRedisTemplate = stringRedisTemplate;
     }
 
+    /**
+     * 生成全局id
+     *
+     * @param keyPrefix
+     * @return
+     */
     public long nextId(String keyPrefix) {
         // 1.生成时间戳
+        // 获取当前时间戳
         LocalDateTime now = LocalDateTime.now();
+        // 将当前时间 now 转换为从1970-01-01T00:00:00Z（UTC）开始的秒数
         long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
         long timestamp = nowSecond - BEGIN_TIMESTAMP;
 
         // 2.生成序列号
         // 2.1.获取当前日期，精确到天
         String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
-        // 2.2.自增长
+        // 2.2.自增长  key 对应的值自增1
         Long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+        log.info("自增长count: {}", count);
 
-        // 3.拼接并返回
+        // 3.拼接并返回 << 32位
         return timestamp << COUNT_BITS | (count == null ? 0 : count);
     }
 }
